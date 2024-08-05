46d1a93aa21ba38aa78cc40603bdc3e10ea5e26d
Mozzhakov <imozzhakov@icloud.com>
2024-08-05 15:00:38 +0300

    Create homework.py

---
+++ homework.py
@@ -0,0 +1,11 @@
+# First task
+def get_days_from_today(date):
+    return
+
+# Second task
+def get_numbers_ticket(min, max, quantity):
+    return
+
+# Third task
+def normalize_phone(phone_number):
+    return
